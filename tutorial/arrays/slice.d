f4b8c6a452a3ad562f4713647b4aa97d079892b6
Axect <edeftg@gmail.com>
2018-06-21 00:19:39 +0900

    Slice

---
+++ tutorial/arrays/slice.d
@@ -0,0 +1,19 @@
+void main() {
+  import std.stdio : writeln;
+
+  int[] test = [3, 9, 11, 7, 2, 76, 90, 6];
+  test.writeln();
+  writeln("First element: ", test[0]);
+  writeln("Last element: ", test[$ - 1]);
+  writeln("Exclude the first two elements: ", test[2 .. $]);
+
+  writeln("Slices are views on the memory: ");
+  auto test2 = test;
+  auto test3 = test.dup;
+  test[] += 1; // increment each element by 1
+  test.writeln;
+  test2.writeln;
+  test3.writeln;
+
+  assert(test[2 .. 2].length == 0);
+}
