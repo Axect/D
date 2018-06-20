e4880f9c60cd47c23213abbef307af8d73956903
Axect <edeftg@gmail.com>
2018-06-20 23:49:27 +0900

    Mutability

---
+++ tutorial/mutability/mut.d
@@ -0,0 +1,20 @@
+module tutorial.mutability.mut;
+
+void main(string[] args) {
+    import std.stdio : writeln;
+
+    int m = 100;
+    writeln("m: ", typeof(m).stringof);
+    m = 10;
+
+    // const pointer to mutable memory
+    // immutable pointer is not allow.
+    const int* cm = &m;
+    writeln("cm: ", typeof(cm).stringof);
+
+    immutable v = 100;
+    writeln("v: ", typeof(v).stringof);
+
+    immutable int* cv = &v;
+    writeln("cv: ", typeof(cv).stringof);
+}
