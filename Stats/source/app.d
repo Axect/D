8a9e1020aae2ea4566fb8a23121aa2eeeb379753
Axect <edeftg@gmail.com>
2018-07-06 14:47:07 +0900

    Restore App.d
    Former-commit-id: d2fcae29d393416636d844bc7527471b3d05b681

--- Stats/source/app.d
+++ Stats/source/app.d
@@ -5,27 +5,27 @@ import data.vector;
 void main() {
     import std.stdio : writeln;
     
-    // auto b = Vector(1,20,2);
-    // b.writeln;
-    // writeln(b + 1);
-    // writeln(b - 1);
-    // writeln(b * 2);
-    // writeln(b / 2);
-    // b.sqrt.writeln;
-    // b.fmap(x => 2 * x - 1).writeln;
-    // b.dot(b).writeln;
+    auto b = Vector(1,20,2);
+    b.writeln;
+    writeln(b + 1);
+    writeln(b - 1);
+    writeln(b * 2);
+    writeln(b / 2);
+    b.sqrt.writeln;
+    b.fmap(x => 2 * x - 1).writeln;
+    b.dot(b).writeln;
 
-    auto c = Matrix(Vector(1,10000), 100, 100, true);
-    auto d = Matrix(Vector(1,10000), 100, 100, true);
-    // c.writeln;
-    // c[1,1].writeln;
-    // writeln(c + 1);
-    // writeln(c - 1);
-    // writeln(c * 2);
-    // writeln(c / 2);
-    // writeln(c ^^ 2);
-    // writeln(c + c);
-    // writeln(c % c);
-    // d.writeln;
+    auto c = Matrix([1,2,3,4], 2, 2, true);
+    auto d = Matrix([1,2,3,4,5,6], 2, 3, true);
+    c.writeln;
+    c[1,1].writeln;
+    writeln(c + 1);
+    writeln(c - 1);
+    writeln(c * 2);
+    writeln(c / 2);
+    writeln(c ^^ 2);
+    writeln(c + c);
+    writeln(c % c);
+    d.writeln;
     writeln(c % d);
 }
