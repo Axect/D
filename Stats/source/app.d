374d3ef826b990a3182618b2047846ca36494311
Axect <edeftg@gmail.com>
2018-07-13 18:07:38 +0900

    Complete cbind
    Former-commit-id: dc621d978f1f375a4f2ecc65ac3007adff9ebbed

--- Stats/source/app.d
+++ Stats/source/app.d
@@ -4,42 +4,54 @@ import data.vector;
 
 void main() {
     import std.stdio : writeln;
+
+    auto a = Matrix([1,2,3,4], 2, 2, true);
+    a.writeln;
+    a.val.writeln;
+    a.byRow.writeln;
     
-    auto b = Vector(1,20,2);
+    auto b = a.makeFalse;
     b.writeln;
-    writeln(b + 1);
-    writeln(b - 1);
-    writeln(b * 2);
-    writeln(b / 2);
-    b.sqrt.writeln;
-    b.fmap(x => 2 * x - 1).writeln;
-    b.dot(b).writeln;
+    b.val.writeln;
+    b.byRow.writeln;
 
-    auto c = Matrix([1,2,3,4], 2, 2, true);
-    auto d = Matrix([1,2,3,4,5,6], 2, 3, true);
-    c.writeln;
-    c.transpose.writeln;
-    c[1,1].writeln;
-    writeln(c + 1);
-    writeln(c - 1);
-    writeln(c * 2);
-    writeln(c / 2);
-    writeln(c ^^ 2);
-    writeln(c + c);
-    writeln(c % c);
-    d.writeln;
-    writeln(c % d);
+    cbind(a,b).writeln;
+    
+    // auto b = Vector(1,20,2);
+    // b.writeln;
+    // writeln(b + 1);
+    // writeln(b - 1);
+    // writeln(b * 2);
+    // writeln(b / 2);
+    // b.sqrt.writeln;
+    // b.fmap(x => 2 * x - 1).writeln;
+    // b.dot(b).writeln;
 
-    auto m = Matrix([[2, -1, -2], [-4, 6, 3], [-4, -2, 8]]);
-    auto res = m.lu();
-    Matrix l = res[0];
-    Matrix u = res[1];
-    l.writeln;
-    u.writeln;
-    m.block(1).writeln;
-    m.block(2).writeln;
-    m.block(3).writeln;
-    m.block(4).writeln;
-    c.det.writeln;
-    m.det.writeln;
+    // auto c = Matrix([1,2,3,4], 2, 2, true);
+    // auto d = Matrix([1,2,3,4,5,6], 2, 3, true);
+    // c.writeln;
+    // c.transpose.writeln;
+    // c[1,1].writeln;
+    // writeln(c + 1);
+    // writeln(c - 1);
+    // writeln(c * 2);
+    // writeln(c / 2);
+    // writeln(c ^^ 2);
+    // writeln(c + c);
+    // writeln(c % c);
+    // d.writeln;
+    // writeln(c % d);
+
+    // auto m = Matrix([[2, -1, -2], [-4, 6, 3], [-4, -2, 8]]);
+    // auto res = m.lu();
+    // Matrix l = res[0];
+    // Matrix u = res[1];
+    // l.writeln;
+    // u.writeln;
+    // m.block(1).writeln;
+    // m.block(2).writeln;
+    // m.block(3).writeln;
+    // m.block(4).writeln;
+    // c.det.writeln;
+    // m.det.writeln;
 }
--- Stats/source/data/vector.d
+++ Stats/source/data/vector.d
@@ -454,6 +454,27 @@ struct Matrix {
   // ===========================================================================
   // Matrix Utils
   // ===========================================================================
+  /++
+    True -> False
+  +/
+  Matrix makeFalse() {
+    assert(this.byRow);
+    double[] v_ref = this.val.comp; // Double array
+    auto r = this.row;
+    auto c = this.col;
+    auto l = r * c - 1;
+    
+    double[] v_con;
+    v_con.length = l + 1;
+
+    foreach(i; 0 .. l) {
+      auto s = (i * c) % l;
+      v_con[i] = v_ref[s];
+    }
+    v_con[l] = v_ref[l];
+
+    return Matrix(v_con, r, c);
+  }
 
   /++
     Transpose
@@ -596,6 +617,40 @@ struct Matrix {
     return target;
   }
 }
+
+// =============================================================================
+// Functions of vector or matrices
+// =============================================================================
+Vector cbind(Vector m, Vector n) {
+  import std.array : join;
+  
+  Vector container;
+
+  container.comp = join([m.comp, n.comp]);
+  return container;
+}
+
+Matrix cbind(Matrix m, Matrix n) {
+  assert(m.row == n.row);
+  Matrix container;
+  container.row = m.row;
+  container.col = m.col + n.col;
+  
+  if (m.byRow && n.byRow) {
+    m = m.makeFalse;
+    n = n.makeFalse;
+  } else if (n.byRow) {
+    n = n.makeFalse;
+  } else if (m.byRow) {
+    m = m.makeFalse;
+  }
+
+  container.val = cbind(m.val, n.val);
+  container.refresh;
+  return container;
+}
+
+
 
 // =============================================================================
 // Array of Array Utils
