4293b05a9edbc19622bb314daafa13be0682e261
Axect <edeftg@gmail.com>
2018-07-20 14:54:15 +0900

    Complete!
    Former-commit-id: 1586dad49fbadb6d6beff1b0cca6f45120ddc2e2

--- Stats/source/app.d
+++ Stats/source/app.d
@@ -5,11 +5,13 @@ import data.vector;
 void main() {
     import std.stdio : writeln;
 
-    immutable Matrix a = Matrix([[1,2],[3,4]]);
+    Matrix a = Matrix([[1,2],[3,4]]);
     a.writeln;
     auto res = a.lu;
     res[1].writeln;
     a.writeln;
     res[1].invU.writeln;
+    a.writeln;
+    a.inv.writeln;
     a.writeln;
 }
