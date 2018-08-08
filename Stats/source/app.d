ceb2480871ff7852a68d4351c2fe1be1bb19eaf1
Axect <edeftg@gmail.com>
2018-08-09 00:51:48 +0900

    Correlation
    Former-commit-id: 180f2566ea25b4d6059d1d52ea95063e06aeb21f

--- Stats/source/app.d
+++ Stats/source/app.d
@@ -8,26 +8,6 @@ import dnum.stats;
 void main() {
   import std.stdio : writeln;
 
-
-  auto b = Tensor([[1,2],[3,4]]);
-  b.writeln;
-
-  b.det.writeln;
-  b.inv.writeln;
-  b.writeln;
-
-  auto a = Tensor([1,2,3,4]);
-  a.writeln;
-  auto c = Tensor([1,2,3,4], false);
-  c.writeln;
-
-  cbind(a,a).writeln;
-  rbind(a,a).writeln;
-
-  cbind(a,a,a).writeln;
-  rbind(a,a,a).writeln;
-
-  cov(c,c).writeln;
-
-  cov(b).writeln;
+  auto a = Tensor([[1,2],[3,4]]);
+  a.cor.writeln;
 }
