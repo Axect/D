eea335fe9b7db66222d1f7c5105f42bf96b27a13
Axect <edeftg@gmail.com>
2018-08-17 00:36:58 +0900

    ML - But more enum
    Former-commit-id: 668e626d952dc40e245eacc7408a1b9ea31554fc

--- Stats/source/dnum/ml.d
+++ Stats/source/dnum/ml.d
@@ -11,12 +11,28 @@ enum Activation {
   ReLU
 }
 
+alias Layers = uint;
+
 struct NN {
-  uint layers = 1;
-  uint current = 0;
-  Tensor input;
-  Tensor output;
-  Tensor[] weights;
+  // =====================================================
+  //  Necessary Structures
+  // =====================================================
+  uint layers = 1; // Except Input
+  uint current = 0; // Current position; smaller than layers
+  int bias = -1; // Bias
+
+  Tensor x;
+  Tensor y;
+  Tensor[] weights; // length = layers
+
+  this(Tensor input, Layers l) {
+    this.x = input;
+    this.layers = l;
+    this.current = 0;
+
+    auto m = input.nrow;
+    auto n = input.ncol;
+  }
 
   void sequential(int m, int n);
   void activation(Activation A);
