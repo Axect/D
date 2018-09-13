debe9d74a8a7f632be25e62a0858d7369bc178d3
Axect <edeftg@gmail.com>
2018-09-14 02:45:38 +0900

    Beautiful Montecarlo
    Former-commit-id: 283cec0a6858e451f3d05f9589fe532c6f7cb819

--- numerical/montecarlo/source/app.d
+++ numerical/montecarlo/source/app.d
@@ -1,27 +1,64 @@
 import std.stdio : writeln;
-import dnum.tensor;
-import dnum.utils;
-import dnum.stats;
+import std.random;
 
 void main() {
   import std.math : PI, abs;
 
-  int n = 100000000;
-  auto a = rand(1,n);
-  auto b = rand(1,n);
-  auto c = rand(1,n);
-
-  double s = 0;
-  foreach (i; 0 .. n) {
-    if (a[0,i]^^2 + b[0,i]^^2 + c[0,i]^^2 <= 1) {
-      s++;
-    }
-  }
-  
-  auto estimate = s / cast(double)n * 8;
+  auto estimate = roll(10);
   auto actual = 4.0/3.0*PI;
 
+  writeln("------------------------------");
+  writeln("           10^1 times         ");
+  writeln("------------------------------");
   writeln("Estimate Volume: ", estimate);
   writeln("Actual Volume:   ", actual);
   writeln("Error (%):       ", abs(estimate - actual) / actual * 100);
+
+  estimate = roll(100);
+  writeln("------------------------------");
+  writeln("           10^2 times         ");
+  writeln("------------------------------");
+  writeln("Estimate Volume: ", estimate);
+  writeln("Actual Volume:   ", actual);
+  writeln("Error (%):       ", abs(estimate - actual) / actual * 100);
+
+  estimate = roll(10000);
+  writeln("------------------------------");
+  writeln("           10^4 times         ");
+  writeln("------------------------------");
+  writeln("Estimate Volume: ", estimate);
+  writeln("Actual Volume:   ", actual);
+  writeln("Error (%):       ", abs(estimate - actual) / actual * 100);
+
+  estimate = roll(1000000);
+  writeln("------------------------------");
+  writeln("           10^6 times         ");
+  writeln("------------------------------");
+  writeln("Estimate Volume: ", estimate);
+  writeln("Actual Volume:   ", actual);
+  writeln("Error (%):       ", abs(estimate - actual) / actual * 100);
+
+  estimate = roll(100000000);
+  writeln("------------------------------");
+  writeln("           10^8 times         ");
+  writeln("------------------------------");
+  writeln("Estimate Volume: ", estimate);
+  writeln("Actual Volume:   ", actual);
+  writeln("Error (%):       ", abs(estimate - actual) / actual * 100);
+}
+
+double roll(int n) {
+  auto rnd = Random(unpredictableSeed);
+
+  double s = 0;
+  double a, b, c;
+  foreach (i; 0 .. n) {
+    a = uniform(0.0, 1.0, rnd);
+    b = uniform(0.0, 1.0, rnd);
+    c = uniform(0.0, 1.0, rnd);
+    if (a ^^ 2 + b ^^ 2 + c ^^ 2 <= 1) {
+      s++;
+    }
+  }
+  return s / cast(double)n * 8;
 }
